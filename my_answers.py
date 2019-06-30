886b1449226dd60e783082e99a38a27150424ec1
Bopamo Osaisai <bopamo@gmail.com>
2019-06-30 20:35:39 +0000

    Updating variables and refactoring code

--- my_answers.py
+++ my_answers.py
@@ -84,6 +84,7 @@ class NeuralNetwork(object):
             delta_weights_h_o: change in weights from hidden to output layers
 
         '''
+        
         #### Implement the backward pass here ####
         ### Backward pass ###
 
@@ -94,17 +95,17 @@ class NeuralNetwork(object):
 #         hidden_error = np.dot( self.weights_hidden_to_output, output_error_term)
         
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = (y - final_outputs)
+        output_error_term = error
         
         hidden_error = np.dot(self.weights_hidden_to_output, output_error_term)
         
         hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
         
         # Weight step (input to hidden)
-        delta_weights_i_h += self.lr * hidden_error_term * X[:,None]
+        delta_weights_i_h += hidden_error_term * X[:,None]
         
         # Weight step (hidden to output)
-        delta_weights_h_o += self.lr * output_error_term * hidden_outputs[:,None]
+        delta_weights_h_o += output_error_term * hidden_outputs[:,None]
         
         return delta_weights_i_h, delta_weights_h_o
 
@@ -118,8 +119,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += delta_weights_h_o/n_records # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += delta_weights_i_h/n_records # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += self.lr * delta_weights_h_o/n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += self.lr * delta_weights_i_h/n_records # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
